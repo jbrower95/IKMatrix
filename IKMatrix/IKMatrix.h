1107691a57494af097a63e677c281a0fb4b0c78a
jbrower <justin_brower@brown.edu>
2013-12-07 21:51:04 -0500

    Initial Commit

---
+++ IKMatrix/IKMatrix.h
@@ -0,0 +1,13 @@
+//
+//  IKMatrix.h
+//  IKMatrix
+//
+//  Created by Justin Brower on 12/7/13.
+//  Copyright (c) 2013 Justin Brower. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IKMatrix : NSObject
+
+@end
---
+++ IKMatrix/IKMatrix.m
@@ -0,0 +1,13 @@
+//
+//  IKMatrix.m
+//  IKMatrix
+//
+//  Created by Justin Brower on 12/7/13.
+//  Copyright (c) 2013 Justin Brower. All rights reserved.
+//
+
+#import "IKMatrix.h"
+
+@implementation IKMatrix
+
+@end
